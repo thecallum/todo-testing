9fe0cda40cfd673d864e08ec90042d764762057d
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2022-09-09 11:02:06 +0100

    Add message for when list empty

--- src/components/taskList.js
+++ src/components/taskList.js
@@ -1,19 +1,21 @@
 import Task from "../components/task";
 
 export default function TaskList({ tasks, toggleCheckbox, deleteTask }) {
+  if (tasks.length === 0)
+    return <div className="tasklist-empty">You have no tasks</div>;
+
   return (
-    <div>
-      <ul className="tasklist">
-        {tasks.map((task, index) => (
-          <Task
-            key={index}
-            task={task}
-            index={index}
-            toggleCheckbox={toggleCheckbox}
-            deleteTask={deleteTask}
-          />
-        ))}
-      </ul>
-    </div>
+    <ul className="tasklist">
+      {tasks.map((task, index) => (
+        <Task
+          key={index}
+          task={task}
+          index={index}
+          toggleCheckbox={toggleCheckbox}
+          deleteTask={deleteTask}
+        />
+      ))}
+    </ul>
   );
 }
+
